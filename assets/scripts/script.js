18d91bf5022c11797e7899da832dd770962bcd9e
Vasinc <lein0.jade@gmail.com>
2023-02-16 20:19:12 +0200

    2 ( header added + all pages )

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,31 @@
-console.log('succes');
+// variables
+const headerUl = document.getElementById('header-ul');
+const burgerMenu = document.querySelector('.burger-menu');
+
+//functions
+function handleHeaderUl () {
+    headerUl.classList.toggle('display-flex');
+    burgerMenu.classList.toggle('burger-change');
+    setTimeout(() => {
+        headerUl.classList.toggle('translateX-0')
+    }, 1);
+    if (headerUl.classList.contains('display-flex')) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow = 'visible'
+    }
+}
+
+//event listeners
+addEventListener('resize', () => {
+    if (innerWidth < 800) return;
+    if (document.body.style.overflow == 'hidden'){
+        document.body.style.overflow = 'visible';
+    }
+})
+
+burgerMenu.addEventListener('click', handleHeaderUl);
+
+headerUl.addEventListener('click', event => {
+    if(event.target.tagName == 'UL') handleHeaderUl();
+})
